9c0429505d67e2b30b621c55bf4d3adf3dccffc4
javier-commercecraft <javier@commercecraft.ai>
2026-05-22 01:06:26 -0400

    feat: update BASIC_ROUTE to use v2 API and add pricing changes documentation

--- src/lib/routes/index.ts
+++ src/lib/routes/index.ts
@@ -1,2 +1,2 @@
-export const BASIC_ROUTE = 'http://psearch.dveloxsoft.com/api/v1'
+export const BASIC_ROUTE = 'https://psearch.dveloxsoft.com/api/v2'
 export const LOCAL_ROUTE = 'http://localhost:3000'
