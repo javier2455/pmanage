526af82b692e7e7c6432bd3d0e8d0c9dd6746718
javier-commercecraft <javier@commercecraft.ai>
2026-06-08 14:41:54 -0400

    fix: update reverse geocode route to use searchRoutes in reverse-geocode utility

--- src/lib/routes/business.ts
+++ src/lib/routes/business.ts
@@ -12,7 +12,6 @@ export const businessRoutes = {
   deleteProductOfMyBusiness: (businessId: string, productId: string) =>
     `${BASIC_ROUTE}/businesses/${businessId}/products/${productId}`,
   getDashboardSummary: `${BASIC_ROUTE}/dashboard/`,
-  reverseGeocode: `${BASIC_ROUTE}/reverse-geocode`,
   settings: (businessId: string) =>
     `${BASIC_ROUTE}/businesses/${businessId}/settings`,
 };
